8eb8fb60978a28c99f77faad5058166bb0853c2b
SufiyanKhan968 <suffkhan968@gmail.com>
2024-07-31 06:06:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$("#btn-1").click(function () {
+  $("#content-1").slideToggle();
+});
+
+$("#btn-2").click(function () {
+  $("#content-2").slideToggle();
+});
+$("#btn-3").click(function () {
+  $("#content-3").slideToggle();
+});
+$("#btn-4").click(function () {
+  $("#content-4").slideToggle();
+});
+$("#btn-5").click(function () {
+  $("#content-5").slideToggle();
+});
+$("#btn-6").click(function () {
+  $("#content-6").slideToggle();
+});
